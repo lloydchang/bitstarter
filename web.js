0ad1d20ced25047038ea28ba21f35a765f3408ac
Lloyd Chang <lloydchang@gmail.com>
2013-07-11 12:20:02 +0800

    resolve TypeError: Cannot call method 'toString' of undefined

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var fscontent = fs.readFileSync('index.html', 'utf-8');
-  response.send(fs.readFileSync('index.html').buf.toString());
+  response.send(fscontent);
 });
 
 var port = process.env.PORT || 5000;
